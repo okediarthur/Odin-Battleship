475a1a807269c06d20d1c8638df089c995a2fdec
okediarthur <okediarthur@gmail.com>
2024-06-26 13:54:43 +0300

    File Structure Setup

---
+++ src/gameboard.js
@@ -0,0 +1,30 @@
+class Gameboard {
+    constructor() {
+        this.grid = Array(10).fill(null).map(() => Array(10).fill(null));
+        this.ships = [];
+        this.missedShots = [];
+    }
+
+    placeShip(ship, x, y){
+        for(let i = o; i < ship.length; i++){
+            this.grid[x][y + i] = ship;
+        }
+        this.ships.push(ship);
+    }
+
+    receiveAttack(x, y){
+        if(this.grid[x][y]){
+            this.grid[x][y].hit();
+            if(this.grid[x][y].isSunk()){
+                this.ships = this.ships.filter(ship => !ship.isSunk());
+            }
+        } else {
+            this.missedShots.push([x, y]);
+        }
+    }
+    allSunk(){
+        return this.ships.length === 0;
+    }
+}
+
+export default Gameboard;
---
+++ src/ship.js
@@ -0,0 +1,19 @@
+class Ship {
+    constructor(length) {
+        this.length = length;
+        this.hits = 0;
+        this.sunk = false;
+    }
+
+    hit(){
+        this.hits++;
+        if(this.hits >= this.length){
+            this.sunk = true;
+        }
+    }
+
+    isSunk(){
+        return this.sunk;
+    }
+}
+export default Ship;
